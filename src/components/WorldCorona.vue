4fd7b7b69daaaf42228c6cd3dcb4b08ded1f02b9
Md Nazmul Hasan <farhapartex@gmail.com>
2020-05-12 00:23:40 +0600

    GlobalAPI integrated with frontend

--- src/components/WorldCorona.vue
+++ src/components/WorldCorona.vue
@@ -3,7 +3,52 @@
     <div class="w-100 main-header">
       <h4 class="p-3">World Corona Update</h4>
     </div>
-    <div class="container-fluid"></div>
+    <div class="container-fluid">
+      <div class="row bd-update justify-content-center">
+        <div class="col-md-3 col-sm-3 col-lg-3 col-xl-3">
+          <div class="w-100 shadow pl-1 pr-1 pt-3 pb-3 mb-5 bg-white rounded">
+            <div class="header pt-2 pb-1 mx-auto total-cases">
+              <h6 class="text-center">Coronavirus Cases</h6>
+            </div>
+            <div class="container-fluid">
+              <div class="row mt-3">
+                <div class="col col-md-12 col-lg-12">
+                  <p class="text-center">{{ globalCoronaData.confirmed }}</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-3 col-sm-3 col-lg-3 col-xl-3">
+          <div class="w-100 shadow pl-1 pr-1 pt-3 pb-3 mb-5 bg-white rounded">
+            <div class="header pt-2 pb-1 mx-auto recover-case">
+              <h6 class="text-center">Recovered</h6>
+            </div>
+            <div class="container-fluid">
+              <div class="row mt-3">
+                <div class="col col-md-12 col-lg-12">
+                  <p class="text-center">{{ globalCoronaData.recovered }}</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-3 col-sm-3 col-lg-3 col-xl-3">
+          <div class="w-100 shadow pl-1 pr-1 pt-3 pb-3 mb-5 bg-white rounded">
+            <div class="header pt-2 pb-1 mx-auto died-cases">
+              <h6 class="text-center">Deaths</h6>
+            </div>
+            <div class="container-fluid">
+              <div class="row mt-3">
+                <div class="col col-md-12 col-lg-12">
+                  <p class="text-center">{{ globalCoronaData.death }}</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -14,13 +59,39 @@ import { Component, Prop, Vue } from "vue-property-decorator";
   components: {}
 })
 export default class WorldCorona extends Vue {
-  @Prop() readonly bdCoronaData: any;
+  @Prop() readonly globalCoronaData: any;
 
   mounted() {
-    console.log(this.bdCoronaData);
+    console.log(this.globalCoronaData);
   }
 }
 </script>
 
 <style>
+.bd-update p {
+  font-size: 14px;
+}
+
+.header {
+  width: 80%;
+}
+
+.total-cases {
+  background: #737373;
+  color: #fff;
+}
+
+.positive-case {
+  background: #4db8ff;
+  color: #fff;
+}
+
+.recover-case {
+  background: #ff1a75;
+  color: #fff;
+}
+.died-cases {
+  background: #99cc00;
+  color: #fff;
+}
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,7 +2,7 @@
   <div class="home">
     <Navigation></Navigation>
     <BDCorona :bdCoronaData="bdCoronaData"></BDCorona>
-    <WorldCorona :bdCoronaData="bdCoronaData"></WorldCorona>
+    <WorldCorona :globalCoronaData="globalCoronaData"></WorldCorona>
   </div>
 </template>
 
@@ -11,8 +11,8 @@ import { Component, Prop, Vue } from "vue-property-decorator";
 import { Getter, Action } from "vuex-class";
 import Navigation from "../components/Navigation.vue";
 import BDCorona from "../components/BDCorona.vue";
-import WorldCorona from "../components/WorldCoro.vue";
-import { BD_CORONA_INFO } from "../store/actions.names";
+import WorldCorona from "../components/WorldCorona.vue";
+import { BD_CORONA_INFO, GLOBAL_CORONA_INFO } from "../store/actions.names";
 
 @Component({
   name: "Base",
@@ -20,9 +20,12 @@ import { BD_CORONA_INFO } from "../store/actions.names";
 })
 export default class HomePage extends Vue {
   @Action(BD_CORONA_INFO) bdCoronaInfo: any;
-  bdCoronaData: any = [];
+  @Action(GLOBAL_CORONA_INFO) globalCoronaInfo: any;
 
-  mounted() {
+  bdCoronaData: any = [];
+  globalCoronaData: any = [];
+
+  getBdInfo() {
     this.bdCoronaInfo()
       .then((result: any) => {
         this.bdCoronaData = result;
@@ -30,6 +33,22 @@ export default class HomePage extends Vue {
       .catch((e: any) => {
         // this.setErrorMessege("Login Failed! Username/Password is wrong");
       });
+  }
+
+  getGlobalInfo() {
+    this.globalCoronaInfo()
+      .then((result: any) => {
+        this.globalCoronaData = result[0];
+        console.log(this.globalCoronaData);
+      })
+      .catch((e: any) => {
+        // this.setErrorMessege("Login Failed! Username/Password is wrong");
+      });
+  }
+
+  mounted() {
+    this.getBdInfo();
+    this.getGlobalInfo();
   }
 }
 </script>
